614a335b5764f25d6587fa8cc74fb8e385390e31
Cindy Au Yeung <2605617A@student.gla.ac.uk>
2021-06-14 01:29:36 +0800

    Update Board and Initialize Class

--- events/Initalize.java
+++ events/Initalize.java
@@ -6,6 +6,9 @@ import akka.actor.ActorRef;
 import commands.BasicCommands;
 import demo.CommandDemo;
 import structures.GameState;
+import structures.basic.Board;
+import structures.basic.Tile;
+import utils.BasicObjectBuilders;
 
 /**
  * Indicates that both the core game loop in the browser is starting, meaning
@@ -24,7 +27,9 @@ public class Initalize implements EventProcessor{
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 		
 		//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
+		Board board = new Board();
 		setUpPlayerHealthMana(out, gameState);
+		displayTiles(out,board);
 		
 	}
 
@@ -38,6 +43,27 @@ public class Initalize implements EventProcessor{
 		BasicCommands.setPlayer2Mana(out, gameState.getPlayer2());
 		try {Thread.sleep(sleepTime);} catch (InterruptedException e) {e.printStackTrace();}
 	}
-}
+	
+	public void displayTiles(ActorRef out, Board board) {//Method to display Tiles
+		
+		int x = board.getX();
+		int y = board.getY();
+		
+		for(int i=1; i<=x; i++) {
+			
+			for(int j =1; j<=y;j++) 
+				{
+				String index = Integer.toString(i)+Integer.toString(j);
+				Tile tile = board.getTile(i, j);
+				BasicCommands.drawTile(out, tile, 0);
+				try {Thread.sleep(sleepTime);} catch (InterruptedException e) {e.printStackTrace();}
+				}			
+		}
+	}
+		
+	}
+	
+	
+
 
 
