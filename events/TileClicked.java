4d4c5352da0ad81b3de8994f51bed5c626d3ba8c
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-06-21 15:20:23 +0100

    minor

--- events/TileClicked.java
+++ events/TileClicked.java
@@ -35,30 +35,18 @@ import structures.basic.UnitAnimationType;
  *
  */
 public class TileClicked implements EventProcessor{
-<<<<<<< HEAD
-
-
-
-
-=======
-	
 	Tile currentTileClicked;
 	
-	
->>>>>>> b1ce9f65bb04b4493ad99af68ec97d7cc131bd8f
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 
 		//orig code
 		int tilex = message.get("tilex").asInt();
 		int tiley = message.get("tiley").asInt();
-<<<<<<< HEAD
 
 		//Get the tile with the clicked position
-		Tile tile= gameState.getBoard().getTile(tilex, tiley);
-=======
+//		Tile tile= gameState.getBoard().getTile(tilex, tiley);
 		currentTileClicked= gameState.getBoard().getTile(tilex, tiley);
->>>>>>> b1ce9f65bb04b4493ad99af68ec97d7cc131bd8f
 		ArrayList<Tile> player1UnitTiles = gameState.getBoard().getPlayer1UnitTiles(); 
 	
 		//Set gameState.setTileClicked() = currentTile for the first round
@@ -68,44 +56,13 @@ public class TileClicked implements EventProcessor{
 		/*First checking whether the unit can move or not
 		  if move and attack is true, there is no valid move tile and attack unit. Emptying the two arrays */
 		
-<<<<<<< HEAD
 		//insert here: play spell card
-		if(gameState.getCardSelected() instanceof SpellCard) { //if a spell card is selected previously
-			if(checkTile(tile,gameState.getBoard().getHighlightedRedTiles())) {
-				
-			}
-		}
+//		if(gameState.getCardSelected() instanceof SpellCard) { //if a spell card is selected previously
+//			if(checkTile(tile,gameState.getBoard().getHighlightedRedTiles())) {
+//				
+//			}
+//		}
 		
-		
-		/*Check that whether the clickedTile, is equal to the previous clicked tile 
-		 * or whether it belongs to the whiteHighlighted Tiles.
-		 * if not, the tiles will be unhighlighted. And update gameState tileClicked variable
-		 */
-		if(!(gameState.getTileClicked()==tile)){
-			if(!(checkTile(tile,gameState.getBoard().getHighlightedWhiteTiles()))){
-				gameState.getBoard().unhighlightRedTiles(out);
-				gameState.getBoard().unhighlightWhiteTiles(out);
-				gameState.setTileClicked(tile);
-			}
-		}
-
-		/*
-		 * Use helped method to check the tileClick belongs to player1 unit or not.
-		 * If true, call the highlightmoveTile to highlight the tiles.
-		 */
-		if(checkTile(tile, player1UnitTiles)) {
-			gameState.setUnitClicked(tile.getUnit());
-			GroupsCommands.highlightMoveTile(out,gameState,tile.getUnit());	
-			gameState.setTileClicked(tile);
-		}
-
-		/*
-		 * Use helped method to check the tileClick belongs to player1 highlightedTiles List.
-		 * 
-		 * If true, unhighlight the tiles and then call the moveUnit method.
-		 */
-		if((checkTile(tile,gameState.getBoard().getHighlightedWhiteTiles()))){
-=======
 		if(gameState.isAttack()==true) {gameState.getBoard().unhighlightRedTiles(out);}
 		if(gameState.isMove()==true)  {gameState.getBoard().unhighlightWhiteTiles(out);}
 
@@ -134,7 +91,6 @@ public class TileClicked implements EventProcessor{
 		 * If true, unhighlight the tiles and then call the moveUnit method.*/
 		 
 		if(checkTile(currentTileClicked,gameState.getBoard().getHighlightedWhiteTiles())){
->>>>>>> b1ce9f65bb04b4493ad99af68ec97d7cc131bd8f
 			gameState.getBoard().unhighlightWhiteTiles(out);
 			GroupsCommands.moveUnit(out, gameState, gameState.getUnitClicked(),currentTileClicked);}
 			
@@ -145,32 +101,15 @@ public class TileClicked implements EventProcessor{
 		//Setting the gameState's TileClicked with the current tile clicked at the end.
 		gameState.setTileClicked(currentTileClicked);
 	}
-<<<<<<< HEAD
-
+	
+	//Helper method
 	public boolean checkTile (Tile tile, ArrayList<Tile> a) {//check whether the tile is in a arraylist.
 		if(a.contains(tile)) {return true;}
 		return false;
 	}
-=======
-			
-	
-	//Helper method
-	
-	public boolean checkTile (Tile tile, ArrayList<Tile> a) {//check whether the tile is in an arraylist.
-		if(a.contains(tile)) {
-		return true;	
-		}return false;}
->>>>>>> b1ce9f65bb04b4493ad99af68ec97d7cc131bd8f
 
-	
-	
-	
 	//Getter and setter
-	
 	public Tile getCurrentTileClicked() {return currentTileClicked;}
 	public void setCurrentTileClicked(Tile currentTileClicked) {this.currentTileClicked = currentTileClicked;}
-	
-
-	
 	
 }
