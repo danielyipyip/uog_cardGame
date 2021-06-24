a5a64cf2eb9da07fa1002e4676a86dda6285fef1
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-06-24 11:39:57 +0100

    implemented end turn

--- events/EndTurnClicked.java
+++ events/EndTurnClicked.java
@@ -3,6 +3,8 @@ package events;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
+import commands.BasicCommands;
+import commands.GroupsCommands;
 import structures.GameState;
 
 /**
@@ -24,6 +26,36 @@ public class EndTurnClicked implements EventProcessor{
 		gameState.unHighlightCard(out);
 		gameState.getBoard().unhighlightRedTiles(out);
 		gameState.getBoard().unhighlightWhiteTiles(out);
+		
+		/////////////////// put it b4 changing current player ///////////////////////////
+		//turn+1 (if player 2 ends turn)
+		if (gameState.getCurrentPlayer()==gameState.getPlayer2()) {
+			gameState.setTurn(gameState.getTurn()+1);
+		}
+		
+		//change player control (1->2; 2->1)
+		if (gameState.getCurrentPlayer()==gameState.getPlayer1()) {
+			gameState.setCurrentPlayer(gameState.getPlayer2());
+		}else {gameState.setCurrentPlayer(gameState.getPlayer1());}
+		
+		
+		//////////////////after change current player///////////////
+		//refill mana
+		if (gameState.getCurrentPlayer()==gameState.getPlayer1()) {
+			gameState.getPlayer1().setMana(Math.min(gameState.getTurn()+1, 10));
+			BasicCommands.setPlayer1Mana(out, gameState.getPlayer1());
+		}else {
+			gameState.getPlayer2().setMana(Math.min(gameState.getTurn()+1, 10));
+			BasicCommands.setPlayer2Mana(out, gameState.getPlayer2());
+		}try {Thread.sleep(sleepTime);} catch (InterruptedException e) {e.printStackTrace();}
+		
+		//draw card
+		if (gameState.getCurrentPlayer()==gameState.getPlayer1()) {
+			gameState.getPlayer1().getMydeck().drawCard();
+			GroupsCommands.drawHand(out, gameState);
+			try {Thread.sleep(sleepTime);} catch (InterruptedException e) {e.printStackTrace();}
+		}else {gameState.getPlayer2().getMydeck().drawCard();}//player 2 no need draw on screen hand
+		
 	}
 
 }
