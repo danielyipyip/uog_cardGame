6f9f75d586aefe5e80b0a3f467b55e49cd2e0d0c
Yupeng Fu <2500338f@student.gla.ac.uk>
2021-07-04 15:19:30 +0000

    Update GameActor.java

--- app/actors/GameActor.java
+++ app/actors/GameActor.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import akka.actor.AbastractActor;
+
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import events.CardClicked;
